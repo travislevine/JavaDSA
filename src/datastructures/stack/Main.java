568410284dc1561848e4e852cacea370700bda31
Travis <sneakyplutosaturn@gmail.com>
2025-08-07 10:07:11 +1000

    test(Stack): Testing pop method. Understand where the top of stack is.

--- src/datastructures/stack/Main.java
+++ src/datastructures/stack/Main.java
@@ -3,17 +3,16 @@ package datastructures.stack;
 public class Main {
 
     public static void main(String[] args) {
-
         Stack myStack = new Stack(7);
         myStack.push(23);
         myStack.push(3);
         myStack.push(11);
         System.out.println("First stack\n");
         myStack.printStack();
-        // Stack:
+        // Stack: 11, 3, 23, 7
 
         myStack.pop();
-        // Stack:
+        // Stack: 3, 23, 7
         System.out.println("Second stack\n");
         myStack.printStack();
     }
